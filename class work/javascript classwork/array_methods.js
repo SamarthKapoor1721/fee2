a56206fb700e9610585da8ec05e9f159343d8bcf
SamarthKapoor1721 <156178712+SamarthKapoor1721@users.noreply.github.com>
2024-09-23 12:50:10 +0530

    Add files via upload

---
+++ class work/javascript classwork/array_methods.js
@@ -0,0 +1,9 @@
+//array methods: map,reduce,filter and sort
+// 1: map:returns a new array
+let num=[1,2,3,4];
+let cube=function (num){
+
+    return num**3;
+};
+console.log(cube(4));
+// let newArray=num.map(cube());
