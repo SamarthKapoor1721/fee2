58b840b21e5d5cd48825c3d6e1247ae878439bbe
SamarthKapoor1721 <156178712+SamarthKapoor1721@users.noreply.github.com>
2024-09-27 12:27:58 +0530

    Add files via upload

---
+++ class work/javascript classwork/DOM/app.js
@@ -0,0 +1,28 @@
+//DOM:we change things dynamically in dom on the other hand things are static in css
+
+//when we use javascript in console in chrome or any browser the data gets lost after refreshing thats why we use javascript in vscode
+
+//Selectors:it used to select the element that is to be changed
+
+//types:
+//1) getelementByid(choosing by id given in html):
+const a = document.getElementById('great');
+a.style.color="red";
+
+document.getElementById("demo").innerHTML = "Hello World"; // this command is used to enter the text under the element inshort make the chnages in the html
+
+//2)getelementByclassname (choosing by class given in html):
+
+// 3) getelementBytagname(choosing by tag given in html):
+
+var x=document.getElementsByTagName('h1');
+x[0].style.color="yellow";
+
+
+
+
+// 4) Queryselector:
+
+
+
+
